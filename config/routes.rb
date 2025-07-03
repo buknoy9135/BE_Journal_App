bf898a06a074bf51efc82b487a8da08735c5cc9b
buknoy9135 <jalilanthony@gmail.com>
2025-07-03 21:15:39 +0800

    2nd commit - replaced tasks controller with categories controller under user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  get "categories/index"
   devise_for :users
-  get "tasks/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "tasks#index"
-  get "/dashboard", to: "tasks#index"
+  root "categories#index"
+
+  resources :categories do
+    resources :tasks
+  end
 end
